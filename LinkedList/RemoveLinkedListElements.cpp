b7df30891229b71c9613a67546d101dac43cc2b4
Ajeet Kumar Jaiswal <ajeet.jaiswal@capriglobal.in>
2024-02-11 18:45:01 +0530

    added 1 more problem sol

---
+++ LinkedList/RemoveLinkedListElements.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *prev = head, *curr = head, *front = head;
+        while(curr){
+            if (curr->val == val) { 
+                if (curr == head) head = head->next;
+                else prev->next = curr->next;
+            } else {
+                prev = curr;
+            }
+            curr = front;
+            front = front ? front->next : NULL;
+        }
+        return head;
+    }
+};

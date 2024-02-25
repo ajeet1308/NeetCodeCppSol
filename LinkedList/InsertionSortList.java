b034ededa774e32f12b09ba5391b2a29833545a2
Ajeet Kumar Jaiswal <66054508+ajeet1308@users.noreply.github.com>
2024-02-25 20:03:32 +0530

    Update InsertionSortList.java

--- LinkedList/InsertionSortList.java
+++ LinkedList/InsertionSortList.java
@@ -9,6 +9,56 @@
  * }
  */
 class Solution {
+    // InsertionSort Algorithm
+    /*
+        TC: O(N^2) SC:O(1)
+        InsertionSort(arr[]) {
+            for i = 1 to arr.length
+                key = arr[i]
+
+                j = i-1;
+                while j >= 0 and arr[j] > key
+                    arr[j+1] = arr[j]
+                    j = j-1
+                
+                arr[j+1] = key
+        }
+     */
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode key = head.next, keyPrev = head;
+
+        while(key != null) {
+            ListNode start = head, startPrev = null;
+            while(start != key && start.val <= key.val) {
+                startPrev = start;
+                start = start.next;
+            }
+            
+            if (key == start) {
+                keyPrev = key;
+                key = key.next;
+                continue;
+            }
+            keyPrev.next = key.next; // Removed key from original linkage
+            key.next = start;
+
+            if (start == head){ // This means we have add element at first position
+                head = key;
+            } else {
+                startPrev.next = key;
+            }
+
+            key = keyPrev.next;
+        }
+
+        return head;
+
+    }
+}
+/********************************************************************************************************/
+class Solution {
     public ListNode insertionSortList(ListNode head) {
         // TC: O(N^2), SC:O(N)
         if (head == null || head.next == null) return head;

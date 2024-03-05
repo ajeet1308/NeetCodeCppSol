afc86fe307283d553492db374b9a4b2a70f41dd6
Ajeet Kumar Jaiswal <66054508+ajeet1308@users.noreply.github.com>
2024-03-06 02:08:15 +0530

    Update BinaryTreeInorderTraversal.java

--- Trees/BinaryTreeInorderTraversal.java
+++ Trees/BinaryTreeInorderTraversal.java
@@ -33,3 +33,28 @@ class Solution {
         return arr;
     }
 }
+
+/**********************************************************************************************************************************/
+
+class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // TC:O(N), SC:O(N)
+        List<Integer> arr = new ArrayList<Integer>();
+        // Since ArrayList is public class and arr is an object copy of this class so we do not incurr with problem of pass by value, here by default it would be passed by reference
+
+        Stack<TreeNode> st = new Stack(); // LIFO
+
+        while(!st.isEmpty() || root != null) {
+            while(root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            arr.add(root.val);
+            root = root.right;
+        }
+
+        return arr;
+    }
+}
